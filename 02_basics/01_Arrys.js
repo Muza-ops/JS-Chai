964bf674e82e20434536458327ebe124cbb13163
Muza-ops <122979643+Muza-ops@users.noreply.github.com>
2024-01-23 09:06:25 +0000

    container config done

---
+++ 02_basics/01_Arrys.js
@@ -0,0 +1,12 @@
+//Arrays
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeros = ["shuperman" , "bedman"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+console.log(myArr[1]);
+
+//Arry Methods
+
+myArr.push(6)
+console.log(myArr);
